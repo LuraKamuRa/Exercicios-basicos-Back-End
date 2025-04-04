f01ec914ea33c7f6793c23339f5a1ae0da3ae15c
Lucas Cespes Pinna <wd.gigabits@gmail.com>
2025-04-03 19:19:55 -0700

    IRRF com function em C

--- C/Tabela IRRF/IRRF.c
+++ C/Tabela IRRF/IRRF.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
+float CalculaDeducao(float salario){
+
+    float deducao;
+    if (salario <= 2259.20) {
+        deducao = 0;
+    }
+    else if (salario <= 2826.65) {
+        deducao = (salario * 7.5 / 100) - 169.44;
+    }
+    else if (salario <= 3751.05) {
+        deducao = (salario * 15 / 100) - 381.44;
+    }
+    else if (salario <= 4664.68) {
+        deducao =(salario * 22.5 / 100) - 662.77;
+    }
+    else {
+        deducao = (salario * 27.5 / 100) - 896.00;
+    }
+    return deducao;
+}
+
+
+
 
 int main()
 {
-    float salario = 2890;
-    float desconto = 0;
+    float salario = 0;
+    printf("Qual e o seu salario? \n");
+    scanf("%f", &salario);
 
-    if (salario <= 2259.20) {
-        printf("Isento de imposto\n");
-    }
-    else if (salario <= 2826.65) {
-        desconto = (salario * 7.5 / 100) - 169.44;
-    }
-    else if (salario <= 3751.05) {
-        desconto = (salario * 15 / 100) - 381.44;
-    }
-    else if (salario <= 4664.68) {
-        desconto = (salario * 22.5 / 100) - 662.77;
-    }
-    else {
-        desconto = (salario * 27.5 / 100) - 896.00;
-    }
 
-    printf("Seu salário: R$ %.2f\n", salario);
-    printf("Desconto IRRF: R$ %.2f\n", desconto);
-    printf("Salário líquido: R$ %.2f\n", salario - desconto);
+
+    printf("Seu salario: R$ %.2f\n", salario);
+    printf("Desconto IRRF: R$ %.2f\n", CalculaDeducao(salario));
+    printf("Salario liquido: R$ %.2f\n", salario - CalculaDeducao(salario));
 
     return 0;
 }
